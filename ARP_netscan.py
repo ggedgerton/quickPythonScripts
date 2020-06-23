756800a785fe3bd688e01b561569204876470722
Gendler <gendler@system76-pc.localdomain>
2020-06-22 21:07:48 -0400

    ARP Scan clean and running

--- ARP_netscan.py
+++ ARP_netscan.py
@@ -16,9 +16,7 @@ def py_scan(ip):
 
     # Now lets catch the responses in a variable
     # note that we timeout after one second so our script doesnt just hang, waiting for a response
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
     responses = scapy.srp(arp_request_packet, timeout=1)[0]
 
     # lets see who responded with a scapy function called .summary()
@@ -30,24 +28,3 @@ def py_scan(ip):
         print("-------------------------------------------")
 
 py_scan("10.0.2.0/24")
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-    responses, ignored = scapy.srp(arp_request_packet, timeout=1)
-
-    # lets see who responded with a scapy function called .summary()
-    print(responses.summary())
-
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-py_scan("10.0.2.1/24")
->>>>>>> Stashed changes
-=======
-py_scan("10.0.2.1/24")
->>>>>>> Stashed changes
-=======
-py_scan("10.0.2.1/24")
->>>>>>> Stashed changes
